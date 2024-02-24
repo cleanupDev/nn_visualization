acd475aaefb80b8127c253c4558b8ab4e0d65bcb
Philip Goddinger <philip.goddinger@gmail.com>
2024-02-24 20:40:19 +0100

    init commit, add basic tailwind layout, add initial sidecard components

---
+++ components/sidecards/LayerControllCard.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+
+const LayerControllCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Layer Control</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>
+          <p>Layer control description</p>
+        </CardDescription>
+      </CardContent>
+      <CardFooter>
+        <Button>Button</Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default LayerControllCard;
