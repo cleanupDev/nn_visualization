11c54cfd40dc486260c48ed675238708a722d979
Philip Goddinger <philip.goddinger@gmail.com>
2024-02-25 22:28:57 +0100

    Add model actions and update ModelStore interface

--- components/store.ts
+++ components/store.ts
@@ -4,8 +4,25 @@ import { create } from 'zustand';
 import * as tf from "@tensorflow/tfjs";
 
 
+interface ModelActions {
+    setModel: (model: tf.LayersModel | null) => void;
+    setNumNeurons: (num: number) => void;
+    setNumLayers: (num: number) => void;
+    setLayers: (layers: { name: string; neurons: number }[]) => void;
+    setNumParams: (num: number | string) => void;
+    setCurrAcc: (num: number | string) => void;
+    setCurrLoss: (num: number | string) => void;
+    setCurrPhase: (phase: string) => void;
+    setCurrEpoch: (num: number) => void;
+    setIsTraining: (isTraining: boolean) => void;
+    updateNumParams: () => void;
+    resetModelData: () => void;
+}
+
 interface ModelInfo {
     model: tf.LayersModel | null;
+    input_neurons: number;
+    output_neurons: number;
     num_neurons: number;
     num_layers: number;
     layers: { name: string; neurons: number }[];
@@ -17,7 +34,10 @@ interface ModelInfo {
     is_training: boolean;
 }
 
-export const useModelStore = create<ModelInfo>((set) => ({
+type ModelStore = ModelInfo & ModelActions;
+
+
+export const useModelStore = create<ModelStore>((set) => ({
     model: null,
     input_neurons: 2,
     output_neurons: 1,
@@ -34,9 +54,9 @@ export const useModelStore = create<ModelInfo>((set) => ({
     setNumNeurons: (num: number) => set({ num_neurons: num }),
     setNumLayers: (num: number) => set({ num_layers: num }),
     setLayers: (layers: { name: string; neurons: number }[]) => set({ layers }),
-    setNumParams: (num: number) => set({ num_params: num }),
-    setCurrAcc: (num: number) => set({ curr_acc: num }),
-    setCurrLoss: (num: number) => set({ curr_loss: num }),
+    setNumParams: (num: string | number) => set({ num_params: num }),
+    setCurrAcc: (num: string | number) => set({ curr_acc: num }),
+    setCurrLoss: (num: string | number) => set({ curr_loss: num }),
     setCurrPhase: (phase: string) => set({ curr_phase: phase }),
     setCurrEpoch: (num: number) => set({ curr_epoch: num }),
     setIsTraining: (isTraining: boolean) => set({ is_training: isTraining }),
