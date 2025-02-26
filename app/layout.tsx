01145b1c5f1c17dbd51f1ee1eaf0f8baaea56691
cleanupDev <philip.goddinger@gmail.com>
2025-02-26 22:33:45 +0100

    fix page name

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NN Visualizer",
+  title: "Look Inside AI",
   description: "Visualize a simple neural network",
 };
 
